b5761e70ba29ae818c67c44ef30f9882b19b7b5b
Mateus Cordeiro <mateuscgc123@gmail.com>
2021-12-02 22:05:35 -0300

    cria scripts com código dos grupos

---
+++ js/grupos/leo.js
@@ -0,0 +1,59 @@
+// Tudo dentro deste script será criado durante a aula.
+
+// Função inicial para preparar o jogo.
+function jogo() {
+	// Preparar o jogo.
+	jogo = document.getElementById("game");
+
+	// - Preparar o personagem.
+	personagem = criarPersonagem();
+	adicionarElementoAoJogo(jogo, personagem);
+
+	// Determina que quando uma tecla for pressioda, função executaAcao deve ser chamada.
+	document.addEventListener("keydown", function(e){executaAcao(e, personagem)})
+	document.addEventListener("keyup", function(e){terminaAcao(e, personagem)})
+
+	// - Preparar o obstaculo.
+	obstaculo = criarObstaculo(20);
+	adicionarElementoAoJogo(jogo, obstaculo);
+	adicionarMovimentoParaEsquerda(obstaculo);
+
+	setInterval(function () {
+	    mudarTamanhoDoObstaculo(obstaculo, Math.floor(Math.random() * 41) + 20);
+	    mudarAlturaDoObstaculo(obstaculo, Math.floor(Math.random() * 2)*35);
+	    atualizarPontuacao(pontuacao);
+	}, 1000);
+
+	// Preparar teste de colisão entre o jogador e os obstáculos.
+	setInterval(function () {
+	    testarColisao(personagem, obstaculo);
+	}, 10);
+
+	pontuacao = criarPontucao();
+	adicionarElementoAoJogo(jogo, pontuacao);
+}
+
+// Função executada quando qualquer tecla é pressionada.
+function executaAcao(e, elemento) {
+	switch(e.code) {
+		case 'Space':
+		case 'ArrowUp':
+			pular(elemento);
+			break;
+		case 'ArrowDown':
+			abaixar(elemento);
+			break;
+	} 
+}
+
+// Função executada quando qualquer tecla é solta.
+function terminaAcao(e, elemento) {
+	switch(e.code) {
+		case 'ArrowDown':
+			levantar(elemento);
+			break;
+	}
+}
+
+// Chama função para iniciar o jogo.
+jogo();
